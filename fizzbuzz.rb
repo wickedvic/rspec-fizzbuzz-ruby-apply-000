5da60745bf0302b3ea2e451c5d9b207626cbf8f7
Vivek Sanghvi <viksipart@gmail.com>
2020-09-01 15:14:56 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,9 +5,9 @@ def fizzbuzz(num)
    "Fizz"
  elsif num % 5 == 0
    "Buzz"
-   elsif num % 3 == 0 && num % 5 == 0
+   elsif (num % 3 == 0 && num % 5 == 0)
    "FizzBuzz"
-   elsif num % 3 != 0 || num % 5 != 0
+   elsif (num % 3 != 0 || num % 5 != 0)
    "nil"
 end
   
