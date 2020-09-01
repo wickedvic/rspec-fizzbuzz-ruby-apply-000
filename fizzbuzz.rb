8ce5ea48005eeda5894c163891b60399881c8193
Vivek Sanghvi <viksipart@gmail.com>
2020-09-01 15:04:19 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(int)
-  if int % 3
+def fizzbuzz(num)
+  if num % 3
     return "Fizz"
-  elsif int % 5
+  elsif num % 5
   return "Buzz"
 end
   
